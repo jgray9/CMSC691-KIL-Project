8601bce1bbe515d692e7614e616ab3e1ea1f726a
jgray9 <jgray9@umbc.edu>
2026-06-09 11:17:29 -0400

    added promise to access pywebview api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
 
 function App() {
+  let api = new Promise(res => {
+    if(window.pywebview !== undefined) res(window.pywebview.api)
+    else setTimeout(() => res(window.pywebview.api), 100);
+  });
 
   return (
     <>
