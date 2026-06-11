ceff62f5d8a0ef54f7caea15109a5169fe292b2e
jgray9 <pengygray@gmail.com>
2026-06-11 13:49:06 -0400

    move dataset buttons to seperate component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,52 @@
 import './App.css'
 
+function DatasetButtons() {
+  return <>
+    <button onClick={
+      () => api
+        .then(api => api.create_dataset('arg'))
+        .catch(e => console.log(`caught error ${e}`))
+    }>Create Dataset</button>
+
+    <button onClick={
+      () => api
+        .then(api => api.get_datasets())
+        .then(console.log)
+        .catch(e => console.log(`caught error ${e}`))
+    }>Get Datasets</button>
+
+    <button onClick={
+      () => api
+        .then(api => api.get_dataset('arg'))
+        .then(console.log)
+        .catch(e => console.log(`caught error ${e}`))
+    }>Get Dataset</button>
+
+    <button onClick={
+      () => api
+        .then(api => api.delete_dataset('arg'))
+        .catch(e => console.log(`caught error ${e}`))
+    }>Delete Dataset</button>
+  </>
+}
+
 function App() {
   let api = new Promise(res => {
     // if api is not loaded it, repeatedly check until it is
-    if(window.pywebview === undefined) {
+    if (window.pywebview === undefined) {
       let id = setInterval(() => {
-        if(window.pywebview === undefined) return;
+        if (window.pywebview === undefined) return;
         res(window.pywebview.api)
         clearTimeout(id);
       }, 10);
-    // if api is loaded, resolve with it without making timer
+      // if api is loaded, resolve with it without making timer
     } else
       res(window.pywebview.api)
   });
 
   return (
     <>
-      TODO: add frontend
-      <button onClick={
-        () => api
-          .then(api => api.create_dataset('arg'))
-          .catch(e => console.log(`caught error ${e}`))
-      }>Create Dataset</button>
-
-      <button onClick={
-        () => api
-          .then(api => api.get_datasets())
-          .then(console.log)
-          .catch(e => console.log(`caught error ${e}`))
-      }>Get Datasets</button>
-
-      <button onClick={
-        () => api
-          .then(api => api.get_dataset('arg'))
-          .then(console.log)
-          .catch(e => console.log(`caught error ${e}`))
-      }>Get Dataset</button>
-
-      <button onClick={
-        () => api
-          .then(api => api.delete_dataset('arg'))
-          .catch(e => console.log(`caught error ${e}`))
-      }>Delete Dataset</button>
+      <DatasetButtons />
     </>
   )
 }
