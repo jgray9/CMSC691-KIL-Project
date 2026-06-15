1f1f9fe91dfe608172c64bfcadabd2b80f907f04
jgray9 <pengygray@gmail.com>
2026-06-15 15:55:52 -0400

    dataset list displayed and updated on creation/deletion

--- frontend/src/components/DatasetList.jsx
+++ frontend/src/components/DatasetList.jsx
@@ -1,28 +1,41 @@
 import { useEffect, useState } from 'react'
 
-function CreateDatasetButton({ api, name }) {
+function CreateDatasetButton({ api, name, callback }) {
   const [isCreating, setIsCreating] = useState(false);
 
   function createDataset() {
     setIsCreating(true);
     api.create_dataset(name).then(res => {
       setIsCreating(false);
+      callback();
     });
   }
 
-  if(isCreating)
+  if (isCreating)
     return <button>Creating...</button>
   return <button onClick={createDataset}>Create Dataset</button>
 }
 
 function DatasetList({ api }) {
   const [datasetName, setDatasetName] = useState('');
+  const [datasetList, setDatasetList] = useState([]);
+
+  function update_datasets() {
+    api.get_datasets().then(setDatasetList);
+    return;
+  }
+
+  useEffect(update_datasets, []);
 
   return <>
-    <CreateDatasetButton api={api} name={datasetName}/>
+    <CreateDatasetButton
+      api={api}
+      name={datasetName}
+      callback={update_datasets}
+    />
 
     <button onClick={
-      () => api.get_datasets().then(console.log)
+      () => api.get_datasets().then(update_datasets)
     }>Get Datasets</button>
 
     <button onClick={
@@ -30,10 +43,14 @@ function DatasetList({ api }) {
     }>Get Dataset</button>
 
     <button onClick={
-      () => api.delete_dataset(datasetName).then(console.log)
+      () => api.delete_dataset(datasetName).then(update_datasets)
     }>Delete Dataset</button>
 
     <input name='dataset-name' onChange={e => setDatasetName(e.target.value)} />
+
+    <ul>
+      {datasetList.map(ds => <li key={ds.name}>{ds.name} - {ds.rows} rows</li>)}
+    </ul>
   </>
 }
 
